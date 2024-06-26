69154efbb659491e7ccacc8d349a3d50565d8177
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-06-26 21:57:30 +0600

    Add files via upload
    26.06.24

---
+++ Contest_(CF) 17/Make_Permutation.cpp
@@ -0,0 +1,65 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define ld long double
+#define yes "YES"
+#define no "NO"
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* Lets GO */
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    sort(v.begin(), v.end());
+    vector<bool> element(n + 1, false);
+    for (int i = 0; i < n; ++i)
+    {
+        bool found = false;
+        for (int j = 1; j <= n; j++)
+        {
+            if (!element[j] && v[i] <= j)
+            {
+                element[j] = true;
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            cout << no;
+            return;
+        }
+    }
+    cout << yes;
+}
+
+int main() {
+    OLD_MAXTAN
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+        cout << "\n";
+    }
+    return 0;
+}
---
+++ Contest_(CF) 17/Yoga_Class.cpp
@@ -0,0 +1,50 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define ld long double
+#define yes "YES"
+#define no "no"
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /*Lets play*/
+    int n, x,y;
+    cin >> n >> x >>y;
+    int session1 = n/2;
+    int hrs = n%2;
+
+    int ans = (session1*y)+(hrs*x);
+    int ans_2 = n*x;
+    int total = max(ans, ans_2);
+    cout << total;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
