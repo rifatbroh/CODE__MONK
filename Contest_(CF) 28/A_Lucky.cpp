f282863c162e890de7bd88580c5e3e7a87c372ce
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-08-07 02:40:27 +0600

    Add files via upload

---
+++ Contest_(CF) 28/A_Lucky.cpp
@@ -0,0 +1,51 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long
+#define ld long double
+#define yes "YES"
+#define no "no"
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /*let's play*/
+    string s;
+    cin >> s;
+
+    int sum1 = s[0]+ s[1]+ s[2];
+    int sum2 = s[3]+ s[4]+ s[5];
+
+    if (sum1 == sum2)
+        cout << "YES";
+    else
+        cout << "NO";
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
