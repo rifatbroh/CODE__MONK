61d1046943dd381b083a6620a90deb721b62507e
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-08-15 01:35:23 +0600

    Add files via upload

---
+++ Contest_(CF) 35/Truth_Teller_And_Liars_101.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+     int t;  
+     cin>>t;  
+     while(t--)  
+     {  
+        int n,m;  
+        cin>>n>>m;  
+            
+        if(n<=m)  
+            cout<<-1<<endl;  
+        else  
+            cout<<m+m+1<<endl; 
+      }  
+        
+      return 0;  
+        
+} 
