27884eb94af11bd19269a7a815faef8335c8fdfd
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-09-28 19:22:35 +0600

    Add files via upload

---
+++ Contest_(CF) 45/A_Ambitious_Kid.cpp
@@ -0,0 +1,24 @@
+/*
+    MD Rifat 
+    
+    country Bangladesh
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n); 
+    for (int i=0; i<n; i++) cin >> v[i];
+
+    for (int i=0; i<n; i++){
+        if (v[i] < 0){
+            v[i] = -v[i];
+        }
+    }
+    sort(v.begin(), v.end());
+    cout << v[0] ;
+    return 0;
+}
