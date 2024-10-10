64ca70415305fd20c4690de80759823002d0c7d0
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-10-10 21:44:17 +0600

    Add files via upload

---
+++ Contest_(CF) 46/D_D.cpp
@@ -0,0 +1,23 @@
+/*
+    MD Rifat 
+    
+    country Bangladesh
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        int cal = n/2;
+        if (cal == 0)
+            cout << "0" <<endl;
+        cout << cal << endl;
+    }
+    return 0;
+}
