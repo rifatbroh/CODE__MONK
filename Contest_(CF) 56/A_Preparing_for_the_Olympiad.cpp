1b536d393d024d7d3d5f0d8cfa46bfa2aa7cb15a
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-12-23 21:49:00 +0600

    Add files via upload

---
+++ Contest_(CF) 56/A_Preparing_for_the_Olympiad.cpp
@@ -0,0 +1,66 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v1(n), v2(n);
+
+    for(auto &x: v1){
+        cin >> x;
+    }
+    for(auto &x: v2){
+        cin >> x;
+    }
+
+
+    ll ans = 0;
+    for (int i = 0; i < n-1; i++)
+    {
+        if(v1[i] > v2[i+1])ans += v1[i] - v2[i+1];
+    }
+    ans += v1[n-1];
+    cout << ans ;
+
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
