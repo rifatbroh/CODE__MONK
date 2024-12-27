abd6079475a93d4b4da3fc971a59ec93dcbaae90
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-12-27 20:49:49 +0600

    Add files via upload

---
+++ Contest_(CF) 58/A_Luntik_and_Concerts.cpp
@@ -0,0 +1,20 @@
+/*
+    MD Rifat 
+    
+    country Bangladesh
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        cout << (a+c)%2 << endl;
+    }
+    return 0;
+}
