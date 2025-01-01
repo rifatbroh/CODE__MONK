f242151fae98dcf9c8c7d47601172afadf25b511
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-01-01 22:41:11 +0600

    Add files via upload

---
+++ Contest_(CF) 59/B_Bad_Boy.cpp
@@ -0,0 +1,57 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int x1, x2, y1, y2;
+    cin >> x1 >> x2 >> y1 >> y2;
+
+    // int a = max(x1+1, y1);
+    // int b = max(x2-1, y2);
+    // int c = max(x1, y1+1);
+    // int d = max(x2, y2-1);
+
+    cout << 1 << " " << 1 << " " << x1 << " " << x2 ;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
