9adb9e117b62f84ab59357d087a3d57bc97f6302
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-01-02 23:27:06 +0600

    Add files via upload

---
+++ Contest_(CF) 57/2024.cpp
@@ -0,0 +1,20 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    /*
+        2024 : This is the last year 
+               of my "Downfall" ;
+
+        2025 : This is the first year
+               of my "Come back" ;
+    */
+    return 0;
+}
