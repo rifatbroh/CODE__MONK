3a617b44b0f88eb18d2858e555dee37b6afe569b
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-01-10 18:39:42 +0600

    Add files via upload

---
+++ Contest_(CF) 60/A_Required_Remainder.cpp
@@ -0,0 +1,60 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int x, y, n;
+    cin >> x >> y >> n;
+
+    int mx = (n/x) * x+y;
+    // for (int i=0; i<n; i++){
+    //     if (i%x == y){
+    //         mx = max(mx, i);
+    //     }
+    // }
+    if (mx > n)
+        mx -= x;
+    cout << mx ;
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
