03a5dd23c3e5fc0bb705a82b533956662276e62d
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-01-20 22:40:14 +0600

    Add files via upload

---
+++ Contest_(CF) 62/B_What_is_the_contest_day.cpp
@@ -0,0 +1,19 @@
+/*
+    MD Rifat 
+    
+    country Bangladesh
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n%2==0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
