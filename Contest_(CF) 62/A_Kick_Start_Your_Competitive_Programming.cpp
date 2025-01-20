0cdec1d61ffaf3f014246e261d1dff546064da09
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-01-20 22:39:36 +0600

    Add files via upload

---
+++ Contest_(CF) 62/A_Kick_Start_Your_Competitive_Programming.cpp
@@ -0,0 +1,18 @@
+/*
+    MD Rifat 
+    
+    country Bangladesh
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    if (s == "programmer")
+        cout << "Hello, programmer" << endl;
+    return 0;
+}
