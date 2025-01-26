97a69981785747ff88177ac9c7056278433c3555
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-01-27 02:08:07 +0600

    Add files via upload

---
+++ Contest_(CF) 63/A_Minimal_Coprime.cpp
@@ -0,0 +1,23 @@
+/*
+    MD Rifat 
+    
+    country Bangladesh
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--) {
+        int a, b;
+        cin >> a >> b;
+
+        if (a == 1 && b == 1)
+            cout << "1" << endl;
+        else
+            cout << abs(a-b) << endl;
+    }
+    return 0;
+}
