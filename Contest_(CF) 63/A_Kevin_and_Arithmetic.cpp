6df2cc00a205b33336e0701a4f1141fa2a287f4f
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-01-28 21:42:58 +0600

    Add files via upload

---
+++ Contest_(CF) 63/A_Kevin_and_Arithmetic.cpp
@@ -0,0 +1,62 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<ll>
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    ll n;
+    cin >> n;
+    vi v(n);
+    ll odd = 0, even = 0;
+
+    for (ll i=1; i<=n; i++) {
+        cin >> v[i];
+        if (v[i]%2 == 0) even++;
+        else odd++;
+    }
+    if (even >=1)
+        cout << 1+odd ;
+    else 
+        cout << odd-1 ;
+ }
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
