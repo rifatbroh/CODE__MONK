4ba214bf040706dc301bd0e5760b5f96308b528f
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-02-13 21:31:13 +0600

    Add files via upload

---
+++ Contest_(CF) 64/A_United_We_Stand.cpp
@@ -0,0 +1,79 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vin(a) for (auto &it : a) cin >> it;
+#define vout(a) for (auto &it : a) cout << it << " ";
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve() {
+    /* _THE^MAXTAN_ */
+    int n;
+    cin >> n;
+    vi v(n), a, b;
+    vin(v);
+
+    bool flag = true;
+    for (int i = 1; i < n; i++) { 
+        if (v[i] != v[i - 1]) {
+            flag = false;
+        }
+    }
+    if (flag) {
+        cout << -1 << endl;
+        return;
+    }
+    sort(vec(v));
+    for (int i = 0; i < n; i++) { 
+        if (v[0] == v[i]) {
+            a.push_back(v[i]);
+        } else {
+            b.push_back(v[i]);
+        }
+    }
+
+    cout << a.size() << " " << b.size() << endl;
+    for (auto x : a) cout << x << " ";
+    cout << endl;
+    for (auto y : b) cout << y << " ";
+    cout << endl;
+}
+
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        //cout<<"\n";
+    }
+    return 0;
+}
