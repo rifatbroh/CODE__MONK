0430f05e4fa56c6867461541d08333c72ebd5b12
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-03-01 21:16:18 +0600

    Add files via upload

---
+++ Contest_(CF) 65/A_Division.cpp
@@ -0,0 +1,57 @@
+/*
+    MD Rifat Hossain
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OLD_MAXTAN                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+#define ll long long
+#define ld long double
+#define yes cout << "YES"
+#define no cout << "NO"
+#define pb push_back
+#define vi vector<int>
+#define vin(a) for (auto &it : a) cin >> it;
+#define vout(a) for (auto &it : a) cout << it << " ";
+#define vp vector<pair<int, int>>
+#define mp map<int, int>
+#define vec(v) v.begin(), v.end()
+/* !    !   !   !   !   !   !   */
+#define PI 3.1415926535897932384626433832795
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve()
+{
+    /* _THE^MAXTAN_*/
+    int n;
+    cin >> n;
+
+    if (n >= 1900) cout << "Division 1";
+    else if (n >=1600 && n<=1899) cout << "Division 2";
+    else if (n >= 1400 && n <= 1599) cout << "Division 3";
+    else cout << "Division 4";
+}
+int main()
+{
+    OLD_MAXTAN
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
