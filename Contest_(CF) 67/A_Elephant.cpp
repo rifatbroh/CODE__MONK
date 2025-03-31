11143e75abf03f2dd37007bd10be6b2a9d2a5161
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-03-31 16:36:36 +0600

    Add files via upload

---
+++ Contest_(CF) 67/A_Elephant.cpp
@@ -0,0 +1,16 @@
+/*
+    MD Rifat 
+    
+    country Bangladesh
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float n;
+    cin >> n;
+    
+    cout << ceil(n/5) << endl;
+    return 0;
+}
