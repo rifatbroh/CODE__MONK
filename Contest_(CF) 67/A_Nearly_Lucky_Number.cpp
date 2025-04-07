73f4b305ccfa4bdbb5ea3ab9b4e8788c9a92a004
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-04-07 20:52:49 +0600

    Add files via upload

---
+++ Contest_(CF) 67/A_Nearly_Lucky_Number.cpp
@@ -0,0 +1,24 @@
+/*
+    MD Rifat 
+    
+    country Bangladesh
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+
+    int cnt = 0;
+    for (int i=0; i<s.size(); i++) {
+        if (s[i] == '4' || s[i] == '7')
+            cnt++;
+    }
+    if (cnt == 4 || cnt == 7)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
