940838472528be55537770ce6a7725de8658b3f3
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-04-23 22:56:57 +0600

    Add files via upload

---
+++ Contest_(CF) 68/A_Calculating_Function.cpp
@@ -0,0 +1,29 @@
+/*
+    MD Rifat 
+    
+    country Bangladesh
+*/
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    // ll ans = 0;
+    // for (ll i=1; i<=n; i++) {
+    //     if (i%2 == 1)
+    //         ans -= i;
+    //     else
+    //         ans += i;
+    // }
+    // cout << ans << endl;
+
+    if (n%2 == 0) 
+        cout << n/2 << endl;
+    else
+        cout << (n/2)-n << endl;
+    return 0;
+}
