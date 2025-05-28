d825d32b4ff37bd7b1b9a27fa9c02379c9b47a72
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-05-28 23:30:46 +0600

    Add files via upload

---
+++ Contest_(CF) 69/A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,23 @@
+/*
+    MD Rifat 
+    
+    country Bangladesh
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, ans;
+        cin >> n;
+        ans = (n - 1) / 2;
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}
